be6b5d796d74210fc66d0247466dce6e8ad9f376
Gustavo Maciel <gu.ga.14@hotmail.com>
2020-09-10 12:02:55 -0300

    First Commit

---
+++ src/main/java/com/gustavomaciel/coursemongodb/CoursemongodbApplication.java
@@ -0,0 +1,13 @@
+package com.gustavomaciel.coursemongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursemongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursemongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavomaciel/coursemongodb/CoursemongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavomaciel.coursemongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursemongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
